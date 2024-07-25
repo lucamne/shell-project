12537fa5db1fdaa284920c5b8fb2d4dd78246a12
lucamne <71897009+lucamne@users.noreply.github.com>
2024-07-25 19:17:12 -0400

    refactored code

--- src/wish.c
+++ src/wish.c
@@ -9,19 +9,44 @@
 char* PATH = NULL;
 
 // print an error message
-int print_error(const char* error_message)
+int print_error(const char* const error_message)
 {
 	write(STDERR_FILENO, error_message, strlen(error_message));
 	return 0;
 }
 
-/* Given an arg_string, updates argv and returns argument count. 
- * argv should be uninitialized or NULL and terminated by a NULL pointer.
- * The user should call clean_args after each call to get_args in order to free memory. */
-int get_args(char* arg_string, char*** argv)
+// searches PATH for program and updates fath to program path if found or NULL otherwise
+// path should be unitilized or NULL and should be later freed if not null
+int find_path(const char* const program, char** path)
 {
-	int argc = 0;
+	char* stringp = PATH;
+	while(stringp != NULL)
+	{
+		// parse out directory
+		const char* const dir = strsep(&stringp, ":");
+		// assemble full path with directory and program
+		*path = malloc(sizeof(char) * (strlen(dir) + strlen(program) + 1));
+		*path = strcpy(*path, dir);
+		*path = strcat(*path, program);
+		// check if file exists and is executable
+		if (!access(*path, X_OK))
+		{
+			return 0;
+		}
+	}
+	// no suitable path was found so set path to NULL and free
+	if (*path != NULL)
+		free(*path);
+	*path = NULL;
+	return 0;
+}
 
+/* Given an arg_string, updates argv and argc.
+ * argv should be uninitialized or NULL and will be terminated by a NULL pointer after return.
+ * The user should call clean_args after each call to get_args in order to free memory. */
+int get_args(char* arg_string, char*** argv, int* argc)
+{
+	*argc = 0;
 	// Calculate the number of arguments
 	char* c = arg_string;
 	bool prev_was_char = false;
@@ -30,7 +55,7 @@ int get_args(char* arg_string, char*** argv)
 		// if c points to the start of a new argument
 		if (!prev_was_char && (*c != ' ' && *c != '\n' && *c != '\t'))  
 		{
-			argc++;
+			(*argc)++;
 			prev_was_char = true;
 		}
 		// else if c points to a space between arguments
@@ -41,7 +66,7 @@ int get_args(char* arg_string, char*** argv)
 		c++;
 	}
 	// allocate argv array according to argument count
-	*argv = malloc((argc + 1) * sizeof(char*));
+	*argv = malloc((*argc + 1) * sizeof(char*));
 
 	// copy tokens into array
 	int curr_arg = 0;
@@ -54,9 +79,9 @@ int get_args(char* arg_string, char*** argv)
 			strcpy((*argv)[curr_arg++], token);
 		}
 	}	
-	(*argv)[argc] = NULL;
+	(*argv)[*argc] = NULL;
 
-	return argc;
+	return 0;
 }
 
 // Call once per call to get_args to cleanup argv
@@ -74,14 +99,18 @@ int clean_args(int argc, char*** argv)
 
 // runs built in commands if applicable
 // returns 1 if a command was run and 0 otherwise
-int run_built_in(int argc, char** argv) 
-{
-	if (argc == 0)
-	{
+// expects NULL terminated array
+int run_built_in(char** const argv) 
+{	
+	// count arguments
+	int argc = 0;
+	while (argv[++argc] != NULL) {}
+	const char* const command = argv[0];
+	if (command == NULL)
 		return 0;
-	}
-
-	char* command = argv[0];
+	// check for exit
+	if (strcmp(command, "exit") == 0)
+		exit(0);
 	// check if cd command was enetered
 	if (strcmp(command, "cd") == 0)
 	{
@@ -106,9 +135,11 @@ int run_built_in(int argc, char** argv)
 			return 1;
 		}
 
+		// determine lenght of path
 		int path_len = 0;
 		for (int i = 1; i < argc; i ++)
 		{
+			// add one to account for deliminating character
 			path_len += strlen(argv[i]) + 1;
 		}
 
@@ -119,133 +150,122 @@ int run_built_in(int argc, char** argv)
 			strcat(PATH, argv[i]);
 			strcat(PATH, ":");
 		}
+		// replace final deliminator with null byte
 		PATH[path_len - 1] = '\0';
 
 		return 1;
 	}
-	// check if exit command was entered
-	if (strcmp(command, "exit") == 0)
-	{
-		exit(0);
-	}
 	return 0;
 }
 
-// given argument list, and execute command
-void exec_command(int argc, char** argv)
+/* populates vector of vector of strings pointed to by commandv with the commands to be run
+* and their respective arguments.
+* Each vector in commmandv is NULL terminated
+* *commandv should be passed in unitialized or NULL. 
+* The char pointers pointed to by argv should not be modified or freed until commandv is no longer needed. */
+int parse_args(int argc, char* const argv[], int* const commandc, char**** const commandv)
 {
-	char* command = argv[0];
-	// return if no commands were entered
-	if (argc == 0)
+	// init cv with space for one char** vector and commandc to 0
+	*commandc = 0;
+	*commandv = NULL;
+	// parse argv into seperate commands deliminated by '&'
+	int curr_argc = 0;
+	for (int i = 0; i <= argc; i++)
 	{
-		exit(0);
-	}
-	// look for program in path and execute
-	char* path_cpy = malloc(sizeof(char) * (strlen(PATH) + 1));
-	strcpy(path_cpy, PATH);
-	while(path_cpy != NULL)
-	{
-		const char* const dir = strsep(&path_cpy, ":");
-		// append command to ith directory in pathv
-		char* const path = malloc(sizeof(char) * (strlen(dir) + strlen(command) + 1));
-		strcpy(path, dir);
-		strcat(path, command);
-
-		if (!access(path, X_OK))
+		// if i is within range set curr_arg, otherwise set to NULL
+		const char* curr_arg = NULL;
+		if (i < argc)
+			curr_arg = argv[i];
+		else
+			curr_arg = NULL;	
+		if (i >= argc || strcmp(curr_arg, "&") == 0)
 		{
-			// prepare execv call
-			// first element of argv should be changed to full path
-			argv[0] = path;
-			// ensure argv is terminated with NULL pointer
-			if (argv[argc - 1] != NULL)
-			{
-				argv = realloc(argv, sizeof(char*) * (argc + 1));
-				argv[argc] = NULL;
-			}
-			free(path_cpy);
-			execv(path, argv);
-			exit(0);
-		}
-	}	
-	free(path_cpy);
-	print_error("Unrecognized command\n");
-	exit(0);
-}
+			// increment commandc
+			(*commandc)++;
+			// add 1 to accomodate terminatig NULL pointer
+			char** new_argv = malloc(sizeof(char*) * (curr_argc + 1));
+			// copy commmand and respective arguments
+			new_argv = memcpy(new_argv, argv + (i - curr_argc), sizeof(char*) * curr_argc);
+			// terminate with NULL pointer
+			new_argv[curr_argc] = NULL;
+			// resize cv
+			*commandv = realloc(*commandv, sizeof(char**) * *commandc);
+			// point next pointer in vector to new_argv
+			(*commandv)[*commandc - 1] = new_argv;
 
-// parses and processes input line
-int process_input(char* input)
-{
-	char** argv = NULL;
-	// parse all arguments from input string into a vectory
-	const int total_argc = get_args(input, &argv);
-	// run built in commands first
-	if (run_built_in(total_argc, argv))
-	{
-		return 0;
-	}
-	// used to when joining child processes
-	int childrenc = 0;
-	int pid = -1;
-	// used to count arguments for each command including the command itself
-	int argc = 0;
-	// used to traverse through wish_argv
-	char** argv_pos = argv;
-	while (*argv_pos != NULL && pid != 0)
-	{
-		if (strcmp(*argv_pos, "&") == 0)
-		{
-			pid = fork();
-			if (pid == -1)
-			{
-				print_error("Fork failed\n");
-			}
-			else if (pid == 0)
-			{
-				// make copy of argv so that exec_command receives a pointer returned by malloc
-				char** argv_cpy = malloc((argc + 1) * sizeof(char*));
-				argv_cpy = memcpy(argv_cpy, argv_pos - argc, (argc + 1) * sizeof(char*));
-				// NULL terminate array
-				argv_cpy[argc] = NULL;
-				// execute command
-				exec_command(argc, argv_cpy);
-				free(argv_cpy);
-			}
-			argc = 0;
-			childrenc++;
+			curr_argc = 0;
+			// increment i so that '&' is not included in arguments
 		}
 		else
 		{
-			argc++;
+			curr_argc++;
 		}
-		argv_pos++;
 	}
-	if (argc > 0 && pid != 0)
+	// NULL terminate *commandv
+	*commandv = realloc(*commandv, sizeof(char**) * (*commandc + 1));
+	(*commandv)[*commandc] = NULL;
+
+	return 0;
+}
+
+// creates child processes and execute commands
+// built_in_commands can be passed in parallel but will not run in parallel
+int exec_commands(const int commandc, char*** const commandv)
+{
+	// counts children to be joined
+	int childc = 0;
+	// iterate through commands
+	for (int i = 0; i < commandc; i++)
 	{
-		pid = fork();
+		// if a built in command runs skip forking process and continue
+		if (run_built_in(commandv[i]))
+			continue;
+		// create child process
+		int pid = fork();
 		if (pid == -1)
 		{
-			print_error("Fork failed\n");
+			print_error("Failed to create child process\n");
+			continue;
 		}
-		else if (pid == 0)
+		// if parent increment child counter
+		else if (pid != 0)
 		{
-			// make copy of argv so that exec_command receives a pointer returned by malloc
-			char** argv_cpy = malloc((argc + 1) * sizeof(char*));
-			argv_cpy = memcpy(argv_cpy, argv_pos - argc, (argc + 1) * sizeof(char*));
-			// NULL terminate array
-			argv_cpy[argc] = NULL;
-			// execute command
-			exec_command(argc, argv_cpy);
-			free(argv_cpy);
+			childc++;
 		}
-		childrenc++;
+		// else execute command
+		else
+		{
+			// attempt to run built in command first
+			if (run_built_in(commandv[i]))
+				exit(0);
+			// otherwise find program in path
+			// find program
+			char* path = NULL;
+			find_path(commandv[i][0], &path);
+			// check if program was found
+			if (path == NULL)
+			{
+				print_error("Unrecognized command\n");
+			}
+			else
+			{
+				// if pointer to command is not NULL free and replace with full path
+				if (commandv[i][0])
+				{
+					free(commandv[i][0]);
+					commandv[i][0] = path;
+					execv(path, commandv[i]);
+				}
+			}
+			// ensure child has exited
+			exit(0);
+		}
 	}
 	// join child processes
-	for (int i = 0; i < childrenc; i++)
+	for (int i = 0; i < childc; i++)
 	{
 		wait(NULL);
 	}
-	clean_args(total_argc, &argv);
-
 	return 0;
 }
 
@@ -254,18 +274,30 @@ int main(int argc, char** argv)
 	// setup PATH
 	PATH = malloc(sizeof(char) * (strlen("/bin/") + 1));
 	strcpy(PATH, "/bin/");
-	// parse commands and arguments
+
 	char* lineptr = NULL;
 	size_t n = 0;
 	while (1)
 	{
 		printf("wish> ");	
+		// get input line
 		if (getline(&lineptr, &n, stdin) == -1)
 		{
 			exit(0);
 		}
-		process_input(lineptr);
+		// split line into arguments
+		char** wargv = NULL;
+		int wargc = 0;
+		get_args(lineptr, &wargv, &wargc);
+		// parse arguments into vector of commands
+		char*** commandv = NULL;
+		int commandc = 0;
+		parse_args(wargc, wargv, &commandc, &commandv);
+		// execute commands
+		exec_commands(commandc, commandv);
+		// cleanup
+		clean_args(wargc, &wargv);
+		// TODO: clean_commands(commandc, &commandv);
 	}	
 	return 0;
 }
-
